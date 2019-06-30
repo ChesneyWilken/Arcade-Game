974cfe33d05fb46d3f484ea32de0dd9d6aa8c24c
Chesney <chezwilken@yahoo.co.uk>
2019-06-30 14:33:32 +0100

    New Feature
    - Added W,A,S,D keyboard functionality.

--- js/app.js
+++ js/app.js
@@ -108,7 +108,11 @@ document.addEventListener('keyup', function(e) {
         37: 'left',
         38: 'up',
         39: 'right',
-        40: 'down'
+        40: 'down',
+        87: 'up',
+        65: 'left',
+        83: 'down',
+        68: 'right'
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
